3c24a5adf03bbc924b5429fe1a76a40bc0876fb4
Kate <kate.dubitski@gmail.com>
2018-02-02 02:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ function logWhisper(str){
 
 function sayHiToGrandma(str){
   var check = str
-  if(check.toLowerCase === str){
+  if(check.toLowerCase() === str){
     return "I can't hear you!"
+  }
+  if(check.toUpperCase() === str){
+    return "YES INDEED!"
+  }
+  else{
+    return "I love you,too."
   }
 }
